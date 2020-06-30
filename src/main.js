a394ef6fdbc73c47cf75dff6e0991a48cc40df89
creendev <developer@creen.io>
2020-06-30 22:10:07 +0200

    Using vuex

--- src/main.js
+++ src/main.js
@@ -1,19 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-//import VueRouter from 'vue-router'
-//import Vuex from 'vuex'
+import VueRouter from 'vue-router'
+import Vuex from 'vuex'
 import VueSweetalert2 from 'vue-sweetalert2';
 
 import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
     router,
+    store,
     render: h => h(App)
 }).$mount('#app')
 
-//Vue.use(VueRouter);
-//Vue.use(Vuex);
+Vue.use(VueRouter);
+Vue.use(Vuex);
 Vue.use(VueSweetalert2);
