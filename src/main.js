be58721bb580b82dc91f812eedd1180a33caaa90
creendev <developer@creen.io>
2020-06-30 22:59:49 +0200

    Added axios, VueAxios

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 
 import VueRouter from 'vue-router'
 import Vuex from 'vuex'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import VueSweetalert2 from 'vue-sweetalert2';
 
 import router from './router'
@@ -18,4 +20,5 @@ new Vue({
 
 Vue.use(VueRouter);
 Vue.use(Vuex);
+Vue.use(VueAxios, axios);
 Vue.use(VueSweetalert2);
