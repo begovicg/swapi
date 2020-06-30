60981803c12f666b232fe439c717465c6a38608b
creendev <developer@creen.io>
2020-06-30 21:28:10 +0200

    Using sweetalert2

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueSweetalert2 from 'vue-sweetalert2';
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
+
+Vue.use(VueSweetalert2);
