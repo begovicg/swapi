293a4682aa86cd2e5a5445bf7458a1695db64eee
creendev <developer@creen.io>
2020-06-30 22:00:53 +0200

    Initial login established UC1

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue-router'
-import Vuex from 'vuex'
+
+//import VueRouter from 'vue-router'
+//import Vuex from 'vuex'
 import VueSweetalert2 from 'vue-sweetalert2';
+
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
-    render: h => h(App),
+    router,
+    render: h => h(App)
 }).$mount('#app')
 
-Vue.use(VueRouter);
-Vue.use(Vuex);
+//Vue.use(VueRouter);
+//Vue.use(Vuex);
 Vue.use(VueSweetalert2);
