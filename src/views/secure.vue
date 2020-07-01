093335d7943dafc0f739d7b95296dc83eacece2e
creendev <developer@creen.io>
2020-07-01 03:16:46 +0200

    Initial data fetching from SWAPI

--- src/views/secure.vue
+++ src/views/secure.vue
@@ -1,26 +1,44 @@
 <template>
-    <div id="secure">
-        <h1>Secure Area</h1>
-        <p>
-            This is a secure area
-        </p>
+  <div id="secure">
+    <h1>Secure Area</h1>
+    <p>This is a secure area</p>
+
+    <div class="columns medium-3" v-for="category in categories" :key="category">
+      <div class="card">
+        <div class="card-divider">{{ category.name }}</div>
+        <div class="card-section">
+          <p>{{ category }}.</p>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'Secure',
-        data() {
-            return {};
-        }
-    }
+
+import axios from 'axios'
+export default {
+  name: "Secure",
+  data() {
+    return {
+      categories: {}
+    };
+  },
+  mounted() {
+    axios.get("https://swapi.dev/api/")
+    .then(response => {
+        // Categories - array of key-value pairs, key is category name, value is API uri
+        this.categories = response.data;
+        })
+  }
+};
 </script>
 
 <style scoped>
-    #secure {
-        background-color: #FFFFFF;
-        border: 1px solid #CCCCCC;
-        padding: 20px;
-        margin-top: 10px;
-    }
+#secure {
+  background-color: #ffffff;
+  border: 1px solid #cccccc;
+  padding: 20px;
+  margin-top: 10px;
+}
 </style>
