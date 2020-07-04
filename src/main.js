a80c01971cbf3706a03cfd058a87cb3d887fb6ce
creendev <developer@creen.io>
2020-07-04 15:43:41 +0200

    Installed vuetify.js

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import VueRouter from 'vue-router'
 import Vuex from 'vuex'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import Vuetify from 'vuetify'
 import VueSweetalert2 from 'vue-sweetalert2';
 
 import router from './router'
@@ -12,16 +13,19 @@ import store from './store'
 
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
 new Vue({
     router,
     store,
+    vuetify,
     render: h => h(App)
 }).$mount('#app')
 
 Vue.use(VueRouter);
 Vue.use(Vuex);
 Vue.use(VueAxios, axios);
+Vue.use(Vuetify);
 Vue.use(VueSweetalert2);
--- src/views/secure.vue
+++ src/views/secure.vue
@@ -1,35 +1,70 @@
 <template>
-  <div id="secure">
+  <div id="secure" class="container">
     <h1>Secure Area</h1>
     <p>This is a secure area</p>
 
-    <div class="columns medium-3" v-for="category in categories" :key="category">
-      <div class="card">
-        <div class="card-divider">{{ category.name }}</div>
-        <div class="card-section">
-          <p>{{ category }}.</p>
+    <section class="callout secondary">
+      <h5 class="text-center">Filter by Category</h5>
+      <form>
+        <div class="row">
+          <div class="large-6 columns">
+            <v-select :options="categories" v-model="activeCategory"></v-select>
+          </div>
+          <div class="medium-6 columns">
+            <a @click="getResources()" class="button expanded">Retrieve</a>
+          </div>
         </div>
-      </div>
-    </div>
+      </form>
+    </section>
   </div>
 </template>
 
 <script>
+const CATEGORIES = [
+  "films",
+  "people",
+  "planets",
+  "species",
+  "starships",
+  "vehicles"
+];
+const SWAPI_base_url = "https://swapi.dev/api/";
 
-import axios from 'axios'
+function buildUrl(url) {
+  return SWAPI_base_url + url;
+}
+
+import axios from "axios";
 export default {
   name: "Secure",
   data() {
     return {
-      categories: {}
+      categories: CATEGORIES,
+      activeCategory: null
     };
   },
   mounted() {
+    // Upon mounting, get resources for chosen category
+    // this.getResources(this.activeCategory);
+    /*
     axios.get("https://swapi.dev/api/")
     .then(response => {
-        // Categories - array of key-value pairs, key is category name, value is API uri
         this.categories = response.data;
         })
+    */
+  },
+  methods: {
+    getResources() {
+      let url = buildUrl(this.activeCategory);
+      axios
+        .get(url)
+        .then(response => {
+          this.results = response;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
   }
 };
 </script>
@@ -40,5 +75,35 @@ export default {
   border: 1px solid #cccccc;
   padding: 20px;
   margin-top: 10px;
+}
+
+.container {
+  padding: 20px 0;
+}
+
+.column:last-child:not(:first-child),
+.columns:last-child:not(:first-child) {
+  float: left;
+}
+
+.main_heading {
+  margin: 10px 0 30px;
+}
+
+.callout {
+  margin-bottom: 40px;
+}
+
+select {
+  text-transform: capitalize;
+}
+
+.loader {
+  text-align: center;
+  padding: 40px;
+}
+
+.uppercase {
+  text-transform: uppercase;
 }
 </style>
